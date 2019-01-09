543f742fa6ed3045a6005b38273bad961aff051f
zachang <zachangdawuda@gmail.com>
2019-01-09 09:31:52 +0100

    feat(login-form): creation of login form

--- src/components/Forms/Auth/LoginForm.js
+++ src/components/Forms/Auth/LoginForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import renderField from './renderField';
+import './style.scss';
 
 const validate = (values) => {
 	const errors = {};
@@ -15,13 +17,72 @@ const validate = (values) => {
 	}
 	return errors;
 };
+
+
 const LoginForm = (props) => {
-	const {handleSubmit} = props;
+	const { handleSubmit, submitting } = props
 	return (
-		<form className="form" onSubmit={handleSubmit}>
-			<h1>Login</h1>
-			<p className="text-muted">Sign In to your account</p>
-		</form>
+		<div 
+			className="container-fluid d-flex flex-column 
+			justify-content-center align-items-center
+			container--height"
+		>
+			<h2 
+				className="d-flex justify-content-center form-auth__title" 
+			>
+				Code Realm
+			</h2>
+	
+			<form 
+				className="form form-auth border"
+				onSubmit={handleSubmit((value) => console.log('weee', value))}
+			>
+
+				<h5 className="mt-4">Log in</h5>
+				<hr className="w-100 h-1" />
+				<div className="form-group form-group--top d-flex">
+					<button  
+						className="btn flex-fill w-25 btn-primary"
+					>
+						Facebook
+					</button>
+					<button 
+						className="btn border-right border-left flex-fill w-25  ml-1 disabled"
+					>
+						Email
+					</button>
+					<button 
+						className="btn flex-fill w-25 btn-danger"
+					>
+						Google
+					</button>
+				</div>
+				
+				<Field 
+					name="email" 
+					component={renderField} 
+					type="email"
+					placeholder="Email"
+				/>
+
+				<Field 
+					name="password" 
+					component={renderField} 
+					type="password" 
+					placeholder="Password"
+				/>
+				
+				<div className="form-group d-flex">
+					<button 
+						type="submit" 
+						className="btn btn-primary flex-grow-1" 
+						disabled={submitting}
+					>
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
 	);
 };
 
--- src/containers/Auth/Login/Login.js
+++ src/containers/Auth/Login/Login.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
+import { SubmissionError } from 'redux-form';
 
 import LoginForm from '../../../components/Forms/Auth/LoginForm'
 
-class LoginComponent extends Component {
 
-	login() {
-	}
+class LoginComponent extends Component {
+	
+
+	// login = (value) => console.log('weee', value); 
 
 	render() {
 		return (
-			<LoginForm handleSubmit={this.login()}/>
+			<LoginForm/>
 		)
 	}
 };
