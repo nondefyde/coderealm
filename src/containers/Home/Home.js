888f7051d5ea0feefec8e1313945b85960d1068f
zachang <zachangdawuda@gmail.com>
2019-01-09 23:22:08 +0100

    home page setup

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
+import {
+	Container,
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './Home.scss';
 
 class HomeComponent extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			isOpen: false
+		};
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+
 	render() {
 		return (
-			<div>Am home</div>
+			<div>
+				<Navbar color="light" light expand="md">
+					<Link to="/" className="nav-link brand-name">Code Realm</Link>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<Link to="/register" className="nav-link">Register</Link>
+							</NavItem>
+							<NavItem>
+								<Link to="/login" className="nav-link">Login</Link>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Navbar>
+
+				<Container className="home-container"/>
+			</div>
 		)
 	}
 }
